3f3a179e17e504637fcf772209e0547478e06bd4
zuyeyang <zuyeyang@gmail.com>
2023-05-08 10:06:42 +0000

    draft

--- src/tcp_sender.cc
+++ src/tcp_sender.cc
@@ -1,53 +1,173 @@
 #include "tcp_sender.hh"
 #include "tcp_config.hh"
+#include "tcp_sender_message.hh"
+#include "wrapping_integers.hh"
 
+#include <cstddef>
+#include <cstdint>
 #include <random>
+#include <sys/types.h>
 
 using namespace std;
 
 /* TCPSender constructor (uses a random ISN if none given) */
 TCPSender::TCPSender( uint64_t initial_RTO_ms, optional<Wrap32> fixed_isn )
-  : isn_( fixed_isn.value_or( Wrap32 { random_device()() } ) ), initial_RTO_ms_( initial_RTO_ms )
+  : isn_( fixed_isn.value_or( Wrap32 { random_device()() } ) )
+  , initial_RTO_ms_( initial_RTO_ms )
+  , timer_( initial_RTO_ms )
 {}
 
 uint64_t TCPSender::sequence_numbers_in_flight() const
 {
   // Your code here.
-  return {};
+  return this->sequence_numbers_in_flight_;
 }
 
 uint64_t TCPSender::consecutive_retransmissions() const
 {
   // Your code here.
-  return {};
+  return this->consecutive_retransmission_;
 }
 
 optional<TCPSenderMessage> TCPSender::maybe_send()
 {
   // Your code here.
+  if ( !segment_buffer_.empty() ) {
+    const TCPSenderMessage res = segment_buffer_.front();
+    segment_buffer_.pop();
+    return res;
+  }
   return {};
 }
-
 void TCPSender::push( Reader& outbound_stream )
 {
   // Your code here.
-  (void)outbound_stream;
+  if ( outbound_stream.has_error() ) {
+    return;
+  }
+  TCPSenderMessage msg;
+  if ( next_sequno_ == 0 ) {
+    msg.SYN = true;
+    SYN_SENT_ = true;
+    send_none_empty_message( msg );
+    return;
+  }
+  /* ????? */
+  if ( next_sequno_ == sequence_numbers_in_flight() ) {
+    return;
+  }
+
+  /* send all string data*/
+  /* if window_size_ == 0, then we treat it as 1*/
+  uint64_t effective_w_size = window_size_ == 0 ? 1 : window_size_;
+  uint64_t reminders = effective_w_size - ( next_sequno_ - ackno_ );
+  while ( reminders ) {
+    TCPSenderMessage new_msg;
+    if ( outbound_stream.is_finished() && !FIN_SENT_ ) {
+      /* send a finish msg*/
+      new_msg.FIN = true;
+      FIN_SENT_ = true;
+      send_none_empty_message( new_msg );
+      return;
+    }
+    if ( outbound_stream.is_finished() ) {
+      /* we have sent the FIN msg*/
+      return;
+    }
+    /* load the data into the msg*/
+    size_t size = std::min( reminders, TCPConfig::MAX_PAYLOAD_SIZE );
+    read( outbound_stream, size, new_msg.payload );
+    if ( new_msg.sequence_length() < effective_w_size && outbound_stream.is_finished() ) {
+      /* this is a finish data msg*/
+      new_msg.FIN = true;
+      FIN_SENT_ = true;
+    }
+    if ( new_msg.sequence_length() == 0 ) {
+      return;
+    }
+    send_none_empty_message( new_msg );
+  }
 }
 
 TCPSenderMessage TCPSender::send_empty_message() const
 {
   // Your code here.
-  return {};
+  TCPSenderMessage msg;
+  msg.seqno = Wrap32::wrap( next_sequno_, isn_ );
+  segment_buffer_.push( msg );
+  return msg;
 }
 
 void TCPSender::receive( const TCPReceiverMessage& msg )
 {
   // Your code here.
-  (void)msg;
+  uint64_t abs_ackno = msg.ackno->unwrap( isn_, next_sequno_ );
+  if ( abs_ackno - next_sequno_ > 0 ) {
+    return;
+  }
+  if ( abs_ackno <= ackno_ ) {
+    return;
+  }
+  if ( abs_ackno > 0 && !SYN_ACKED_ ) {
+    SYN_ACKED_ = true;
+  }
+
+  if ( abs_ackno == next_sequno_ && FIN_SENT_ ) {
+    FIN_ACKED_ = true;
+  }
+  window_size_ = msg.window_size;
+  ackno_ = abs_ackno;
+
+  timer_.resetRTO();
+  consecutive_retransmission_ = 0;
+
+  /* update the outstanding queue*/
+  while ( !segment_outstanding_.empty() ) {
+    const auto& sender_msg = segment_outstanding_.front();
+    if ( ackno_ - sender_msg.seqno.unwrap( isn_, ackno_ ) > sender_msg.sequence_length() ) {
+      sequence_numbers_in_flight_ -= sender_msg.sequence_length();
+      segment_outstanding_.pop();
+    } else {
+      break;
+    }
+  }
+
+  if ( !segment_outstanding_.empty() ) {
+    timer_.start();
+  }
 }
 
-void TCPSender::tick( const size_t ms_since_last_tick )
+void TCPSender::tick( uint64_t ms_since_last_tick )
 {
-  // Your code here.
-  (void)ms_since_last_tick;
+  if ( !timer_.ticking( ms_since_last_tick ) ) {
+    return;
+  }
+
+  if ( !segment_outstanding_.empty() ) {
+    segment_buffer_.push( segment_outstanding_.front() );
+    if ( window_size_ != 0 ) {
+      consecutive_retransmission_++;
+      timer_.doubleRTO();
+    }
+    if ( !timer_.isOpen() ) {
+      timer_.start();
+    }
+    if ( SYN_SENT_ && ( next_sequno_ == sequence_numbers_in_flight_ ) && ( timer_.RTO() < 4000 ) ) {
+      timer_.setRTO( 4000 );
+    }
+  } else {
+    /* empty segment outstanding */
+    timer_.close();
+  }
 }
+
+/* Helper Function (Private)*/
+void TCPSender::send_none_empty_message( TCPSenderMessage& msg )
+{
+  msg.seqno = Wrap32::wrap( next_sequno_, isn_ );
+  next_sequno_ += msg.sequence_length();
+  sequence_numbers_in_flight_ += msg.sequence_length();
+
+  segment_buffer_.push( msg );
+  segment_outstanding_.push( msg );
+};
--- src/tcp_sender.hh
+++ src/tcp_sender.hh
@@ -3,11 +3,96 @@
 #include "byte_stream.hh"
 #include "tcp_receiver_message.hh"
 #include "tcp_sender_message.hh"
+#include "wrapping_integers.hh"
+#include <cstddef>
+#include <cstdint>
+#include <vector>
+class TCPTimer
+{
+private:
+  unsigned int initial_RTO_; /* Init RTO*/
+  unsigned int RTO_;         /* Track the upper bounds of RTO*/
+  unsigned int curr_RTO_ {}; /* current RTO*/
+  bool open_ {};
 
+public:
+  //! Initialize a TCP retransmission timer
+  explicit TCPTimer( const uint64_t initial_RTO_ms )
+    : initial_RTO_( initial_RTO_ms ), RTO_( initial_RTO_ ), open_( true )
+  {}
+
+  bool isOpen() { return open_; }
+  unsigned int RTO() const { return RTO_; }
+  void doubleRTO() { RTO_ *= 2; }
+  void setRTO( unsigned int val ) { RTO_ = val; }
+  void resetRTO()
+  {
+    RTO_ = initial_RTO_;
+    curr_RTO_ = 0;
+  }
+  void start()
+  {
+    open_ = true;
+    curr_RTO_ = 0;
+  }
+  void close()
+  {
+    open_ = false;
+    curr_RTO_ = 0;
+  }
+  bool ticking( const size_t& ms_since_last_tick )
+  {
+    if ( isOpen() ) {
+      if ( ms_since_last_tick > RTO_ - curr_RTO_ ) { /* retransmit */
+        curr_RTO_ = RTO_;                            /* reset curr_RTO_*/
+      } else {                                       /* update curr_RTO_*/
+        curr_RTO_ += ms_since_last_tick;
+      }
+      /* !!!!!!!!!!! */
+      /* check if needed! */
+      if ( curr_RTO_ >= RTO_ && open_ ) {
+        curr_RTO_ = 0;
+        return true;
+      }
+      /* !!!!!!!!!!! */
+    }
+    return false;
+  }
+};
 class TCPSender
 {
+  /*!!!!!!!!!!!!!!!!!!*/
+  /* REMOVE REPETITIVE ATTRIBUTES!!!!!!!!!!!!!!!!!!*/
+  /*!!!!!!!!!!!!!!!!!!*/
+private:
   Wrap32 isn_;
   uint64_t initial_RTO_ms_;
+
+  /* storage of TCPSenderMessage*/
+  std::queue<TCPSenderMessage> mutable segment_buffer_ {}; /* TCPSender is going to send*/
+
+  /* State check*/
+  bool SYN_SENT_ { false };
+  bool SYN_ACKED_ { false };
+  bool FIN_SENT_ { false };
+  bool FIN_ACKED_ { false };
+
+  /* Absolute sequece number*/
+  uint64_t next_sequno_ { 0 };
+  uint64_t window_size_ { 0 };
+  uint64_t ackno_ { 0 };
+  uint64_t sequence_numbers_in_flight_ { 0 };
+
+  /* RTO states */
+  TCPTimer timer_;
+  size_t consecutive_retransmission_ { 0 };
+  std::queue<TCPSenderMessage> segment_outstanding_ {}; /*TCPSender may resend*/
+  bool zero_window_ { false };
+
+  /*Helper Function*/
+  uint64_t next_seqno_absolute() const { return next_sequno_; };
+  Wrap32 next_seqno() const { return Wrap32::wrap( next_sequno_, isn_ ); };
+  void send_none_empty_message( TCPSenderMessage& msg );
 
 public:
   /* Construct TCP sender with given default Retransmission Timeout and possible ISN */
